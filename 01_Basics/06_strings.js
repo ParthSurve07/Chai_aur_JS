5b457948190cd59f00e98d1059b2b297dcf807e0
Parth Surve <surveparth429@gmail.com>
2024-10-02 16:13:05 +0530

    String methods

---
+++ 01_Basics/06_strings.js
@@ -0,0 +1,26 @@
+let name = "Parth"
+let age = 19
+
+// String interpolation
+// console.log(`My name is ${name} and I am ${age} years old.`)
+
+const gameName = new String("parthss");
+// console.log(gameName)
+// console.log(gameName.length)
+
+const str1 = gameName.substring(0, 4);
+// console.log(str1)
+
+const str2 = gameName.slice(-5, 6);
+// console.log(str2)
+
+const str3 = "   parth   "
+// console.log(str3)
+// console.log(str3.trim())
+
+
+const str4 = "https://www.google.com/parth%20surve"
+// console.log(str4.replace("%20", "-"))
+
+const str5 = "parth sanjay surve"
+console.log(str5.split(" "))
