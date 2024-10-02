02c0cdb69379f1ccdf1cfbaf1d5dcdc787e338a2
Parth Surve <surveparth429@gmail.com>
2024-10-02 15:53:01 +0530

    Datatypes summary

--- 01_Basics/05_datatypes_summary.js
+++ 01_Basics/05_datatypes_summary.js
@@ -13,3 +13,27 @@
 // 1. Object
 // 2. Array
 // 3. Function
+
+
+// ************************ Memory Allocation ************************
+// Stack memory
+// All premitive data types are stored in stack memory
+// We get copy of the value in stack memory
+// Example
+let name = "parth"
+let name2 = name
+name2 = "parth2"
+console.log(name)
+console.log(name2)
+
+// Heap memory
+// All non-premitive data types are stored in heap memory
+// We get reference of the value in heap memory
+// Example
+let obj = {
+    name: "parth"
+}
+let obj2 = obj
+obj2.name = "parth2"
+console.log(obj.name)
+console.log(obj2.name)
