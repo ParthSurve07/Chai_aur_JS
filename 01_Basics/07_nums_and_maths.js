cff7f20fc33273d6001ee601fb84f6c9d283b6a7
Parth Surve <surveparth429@gmail.com>
2024-10-02 19:01:05 +0530

    Math and num

---
+++ 01_Basics/07_nums_and_maths.js
@@ -0,0 +1,20 @@
+const score = 100
+// console.log(score);
+
+const balance = new Number(35000)
+// console.log(balance)
+
+// console.log(balance.toString())
+// console.log(typeof balance.toString())
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString("en-US"))
+
+
+// Maths
+// console.log((Math.random() * 10) + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min))
