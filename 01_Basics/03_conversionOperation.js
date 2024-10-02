4dd13fad0c64f8c7b5d7c4df700c108d4f3ed9a5
Parth Surve <surveparth429@gmail.com>
2024-10-02 15:25:38 +0530

    Extra conversion operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,3 +1,23 @@
-let score = 33;
+// let someNumber = 22
 
-console.log(typeof(score))
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// Operations
+let value = 3
+let negValue = -value
+
+// console.log(negValue)
+
+let str1 = "Hello"
+let str2 = " Parth"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+console.log('1' + 2)
+console.log(1 + '2')
+console.log('1' + '2')
+console.log('1' + 2 + 2)
+console.log(1 + 2 + '2')
