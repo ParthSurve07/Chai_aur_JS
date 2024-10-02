aef2b6cd4f3daf7c0e07466c2d2844f3d47272f9
Parth Surve <surveparth429@gmail.com>
2024-10-02 15:42:44 +0530

    Datatypes summary

---
+++ 01_Basics/05_datatypes_summary.js
@@ -0,0 +1,15 @@
+// Primitive Data Types
+// This is done using call by value method i.e the value is copied to the new variable and the original value is not changed
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// 6. BigInt
+// 7. Symbol
+
+// Non-Primitive Data Types or Reference Data Types
+// This is done using call by reference method i.e the reference is copied to the new variable and the original value is changed
+// 1. Object
+// 2. Array
+// 3. Function
