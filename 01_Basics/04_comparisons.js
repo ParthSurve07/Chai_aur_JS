1448b1c376ecdd3c5029ff51f8ee9a1a5c151977
Parth Surve <surveparth429@gmail.com>
2024-10-02 15:33:37 +0530

    Comparison operators

---
+++ 01_Basics/04_comparisons.js
@@ -0,0 +1,12 @@
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// Equality operator (==) does not consider the type of the operands
+// but when we use >= or <=, it converts the operands to numbers
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+// === is the strict equality operator
+// It checks the type of the operands as well as the value
