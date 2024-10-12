cc074cf371f3616ba5b5b73ccd32c6276c0b9691
Parth Surve <surveparth429@gmail.com>
2024-10-12 13:48:29 +0530

    Bmi calculator

--- 07_projects/02_bmi-calculator/script.js
+++ 07_projects/02_bmi-calculator/script.js
@@ -1,22 +1,41 @@
 
 
-const submit = document.querySelector('#subBtn')
+const form = document.querySelector('form')
 
 
-submit.addEventListener("click", function() {
-    console.log("Button clicked")
-    const weight = parseInt(document.querySelector('#weight').value)
-    const height = parseInt(document.querySelector('#height').value)
-    const result = parseInt(document.querySelector('#result').value)
+form.addEventListener("submit", function(e) {
+    e.preventDefault()
+
+    const weight = parseInt(document.querySelector("#weight").value)
+    const height = parseInt(document.querySelector("#height").value)
+    const result = document.querySelector("#result")
+    const rcontainer = document.querySelector("#rcontainer")
 
     if (weight === "" || weight < 0 || isNaN(weight)) {
-        result.textContent = "Please enter a valid weight"
+        result.innerHTML = "Please give a valid weight"
     }
     else if (height === "" || height < 0 || isNaN(height)) {
-        result.textContent = "Please enter a valid height"
+        result.innerHTML = "Please give a valid height"
     }
     else {
-        const bmi = weight / (height * height);
-        result.textContent = `Your BMI is ${bmi}`
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2)
+        result.innerHTML = `<span>Your BMI is ${bmi}</span>`
+
+        const div = document.createElement("div")
+        div.classList.add = "text-2xl"
+        div.classList.add = "text-white"
+
+        if (bmi < 18.6) {
+            div.innerHTML = `<span>You are underweight</span>`
+            rcontainer.appendChild(div)
+        }
+        else if (bmi >= 18.6 && bmi <= 24.9) {
+            div.innerHTML = `<span>You are in normal range</span>`
+            rcontainer.appendChild(div)
+        }
+        else {
+            div.innerHTML = `<span>You are overweight</span>`
+            rcontainer.appendChild(div)
+        }
     }
 })
